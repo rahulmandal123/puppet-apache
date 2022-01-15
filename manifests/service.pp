7d17894c02ee6ca56b06c47030281d76479c33d9
root <root@e893560a961c.mylabserver.com>
2022-01-15 14:22:30 +0000

    Added vhosts defined types

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,6 +2,7 @@
 #Allows for the Apache Service to restart when triggered 
 class apache::service {
   service { "${apache::service_name}":
+    alias => 'apache_service',
     ensure => $apache::service_ensure,
     enable => $apache::service_enable,
     hasrestart => true,
